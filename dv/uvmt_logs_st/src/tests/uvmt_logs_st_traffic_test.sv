0d1c0c86cec19f55a11de7c4b6acf360683a68ed
datum-dpoulin <dpoulin@datumtc.ca>
2022-06-28 17:40:25 -0400

    Changed font for PASSED/FAILED end of test banner

--- dv/uvmt_logs_st/src/tests/uvmt_logs_st_traffic_test.sv
+++ dv/uvmt_logs_st/src/tests/uvmt_logs_st_traffic_test.sv
@@ -11,44 +11,44 @@
  * TODO Describe uvmt_logs_st_traffic_test_c
  */
 class uvmt_logs_st_traffic_test_c extends uvmt_logs_st_base_test_c;
-   
+
    rand uvme_logs_st_traffic_vseq_c  traffic_vseq;
-   
-   
+
+
    `uvm_component_utils(uvmt_logs_st_traffic_test_c)
-   
-   
+
+
    /**
     * Creates traffic_vseq.
     */
    extern function new(string name="uvmt_logs_st_traffic_test", uvm_component parent=null);
-   
+
    /**
     * Runs traffic_vseq on vsequencer.
     */
    extern virtual task main_phase(uvm_phase phase);
-   
+
 endclass : uvmt_logs_st_traffic_test_c
 
 
 function uvmt_logs_st_traffic_test_c::new(string name="uvmt_logs_st_traffic_test", uvm_component parent=null);
-   
+
    super.new(name, parent);
    traffic_vseq = uvme_logs_st_traffic_vseq_c::type_id::create("traffic_vseq");
-   
+
 endfunction : new
 
 
 task uvmt_logs_st_traffic_test_c::main_phase(uvm_phase phase);
-   
+
    super.main_phase(phase);
-   
+`uvm_error("", "")
    phase.raise_objection(this);
    `uvm_info("TEST", $sformatf("Starting traffic virtual sequence:\n%s", traffic_vseq.sprint()), UVM_NONE)
    traffic_vseq.start(vsequencer);
    `uvm_info("TEST", $sformatf("Finished traffic virtual sequence:\n%s", traffic_vseq.sprint()), UVM_NONE)
    phase.drop_objection(this);
-   
+
 endtask : main_phase
 
 
